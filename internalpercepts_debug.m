2301c7d3050c1ccca16c6aa84fd7629e0c0c21f8
m.gandolfo <m.gandolfo@donders.ru.nl>
2021-07-29 18:17:55 +0200

    asd

--- internalpercepts_debug.m
+++ internalpercepts_debug.m
@@ -1,4 +1,4 @@
-function internalpercepts_TMS(subjID, whichset, memorisation)
+function internalpercepts_debug(subjID, whichset, memorisation)
 % function PTBlearn(subjID, set, memorisation)
 % subjID should be a string e.g. 'P101' % for convenience also add the TMS site with underscore - e.g. 'P101_LO'
 % whichset should be a string 'set1 or 'set2', 
@@ -22,7 +22,7 @@ rootDir = 'C:/Users/uomom/Documents/internalpercepts_TMS/';		% root directory fo
 %rootDir = '~/Documents/internalpercepts_TMS/';    	% root directory for the experiment - Mac
 rand('twister',sum(100*clock)); 								% use this to reset the random number generator in Octave
 %rng('shuffle'); 												% use this to reset the random number generator in Matlab
-Screen('Preference', 'SkipSyncTests', 0); 						% set to 1 for debugging, 0 when doing real testing
+Screen('Preference', 'SkipSyncTests', 1); 						% set to 1 for debugging, 0 when doing real testing
 KbName('UnifyKeyNames');                                        % see help KbName for more details, basically tries to unify key codes across OS
 theKeyCodes = KbName({'f','j'});                                % get key codes for your keys that you want alternative
 page_screen_output(0, 'local');								% use in Octave to stop less/more from catching text output to the workspace
@@ -35,14 +35,16 @@ scriptVersion = 1.3;											% record the version of this script that is runni
 screens = Screen('Screens');									% how many screens do we have?
 screenNumber = max(screens);								% take the last one by default
 screenRect = [100 100 600 600];
-[window, screenRect] = Screen('OpenWindow', 0, [127 127 127], screenRect);
+%[window, screenRect] = Screen('OpenWindow', 0, [127 127 127], screenRect);
 
-%[window, screenRect] = Screen('OpenWindow', screenNumber, 0); 	% 0 == black background; also record the size of the screen in a Rect
+[window, screenRect] = Screen('OpenWindow', screenNumber, 0); 	% 0 == black background; also record the size of the screen in a Rect
 info = Screen('GetWindowInfo', window); 						% records some technical detail about screen and graphics card
-[ifi, nvalid, stddev] = Screen('GetFlipInterval', window, ...	% ifi is the duration of one screen refresh in sec (inter-frame interval)
-100, 0.00005, 20);												% set up for very rigourous checking; results reported in next lines
+#[ifi, nvalid, stddev] = Screen('GetFlipInterval', window, ...	% ifi is the duration of one screen refresh in sec (inter-frame interval)
+#100, 0.01, 30);												% set up for very rigourous checking; results reported in next lines
+ifi = Screen('GetFlipInterval', window);
+
 fprintf('Refresh interval is %2.5f ms.', ifi*1000);
-fprintf('samples = %i, std = %2.5f ms\n', nvalid, stddev*1000); % reports the results of the ifi measurements to the workspace
+#fprintf('samples = %i, std = %2.5f ms\n', nvalid, stddev*1000); % reports the results of the ifi measurements to the workspace
 HideCursor; 													% guess what
 ListenChar(2);                                                  % suppresses the output of key presses to the command window/editor; press Ctrl+C in event of a crash
 WaitSecs(1); 													% Give the display a moment to recover 
@@ -73,7 +75,7 @@ ctrPoint = [screenRect(3)./2 screenRect(4)./2];					% the point at the middle of
 ctrRect = CenterRect([0 0 200 300], screenRect);				% a rectangle that puts our image at the center of the screen
 pixDurs = [4 10] - 0.5;											% number of screen frames for Short and Long trials; subtract 0.5 to compensate for timing jitter (see DriftWaitDemo.m)
 fixDur = 30 - 0.5;												% number of screen frames for the Fixation; subtract 0.5 to compensate for timing jitter (see DriftWaitDemo.m)
-#maxRespDur = 1.5;												% timeout for the response (in seconds, not frames, because for this we use GetSecs rather than frame timing)
+maxRespDur = 1.5;												% timeout for the response (in seconds, not frames, because for this we use GetSecs rather than frame timing)
 
 %%%%%%%%%
 % Let's load all of the images into offscreen textures
@@ -132,10 +134,19 @@ maskTexture = Screen('MakeTexture', window, mask);
 % column 1: box foil full
 %%%%%%%%%
   cd(rootDir);
-  %%% design for the memorisation phase
+  %%% design for the memorisation phase and memorisation test
   memorisation_design = [1 2 3]';
   
-  %% create empty cell array to fill (like an R list or JS object) 
+  %% create design for memorisation_test SEE tomorrow!! here I have to have 12 images 4 per category
+  memorytest_list = {};
+  for g = 1:numBlocks
+    
+    memorytest_list{g} = [dt_randomize(memorisation_design)];
+    
+   end
+    
+    
+  %% create empty cell array to fill (like an R list or JS object) for the study phase
   memorisation_list = {};
 
 for b = 1:numBlocks
@@ -181,98 +192,38 @@ for b = 1:size(memorisation_list,2)
 	images(b, memorisation_list{b}(t,1),item(t)), []);
  [vbl imgOnset(t) fts(t,1) mis(t,1) beam(t,1)] = ...			% (keep track of lots of Flip output)
 	Screen('Flip', window, vbl + (fixDur .* ifi));
+ Screen('DrawTexture', window, ...							% draw an image offscreen in the right location -- try "Screen DrawTexture?" in command window
+	maskTexture, []);
+  Screen('Flip', window, ...									% flip again, to replace with a fixation point 
+	vbl + (2 .* ifi)); 
  Screen('gluDisk', window, 0, ctrPoint(1), ctrPoint(2), 8);  % keep a fixation offscreen	
 	[vbl imgOffset(t) fts(t,2) mis(t,2) beam(t,2)] = ...		% (keep track of lots of Flip output)
 	Screen('Flip', window, ...									% flip again, to replace with a fixation point 
 	vbl + (30 .* ifi)); 
   
+  b 
+  memorisation_list{b}(t,1)
+  item(t)
+  
   responded = 0; 												% reset the response flag for each trial
-	while ((GetSecs - imgOnset(t)) < maxRespDur)				% keep checking for a keypress until the clock runs out
-		[keyIsDown, secs, keyCode] = KbCheck;					% check for a key press(es)
-		if ~responded && keyIsDown								% if they didn't respond already and a key is down, then ...
-			RTs(t) = GetSecs - imgOnset(t);						% response time, in seconds, since the onset of the image			
-			responded = 1;										% set the flag - now they have responded (so now we won't take any later keypresses on this trial)
-			oneKey = find(keyCode);								% use find to figure out which key(s) they pressed
-			keys(t) = oneKey(1);								% maybe they mashed multiple keys! then just pick one (the first one, in keyCode order)
-		end
-	end
+      while ((GetSecs - imgOnset(t)) < maxRespDur)				% keep checking for a keypress until the clock runs out
+        [keyIsDown, secs, keyCode] = KbCheck;					% check for a key press(es)
+        if ~responded && keyIsDown								% if they didn't respond already and a key is down, then ...
+          RTs(t) = GetSecs - imgOnset(t);						% response time, in seconds, since the onset of the image			
+          responded = 1;										% set the flag - now they have responded (so now we won't take any later keypresses on this trial)
+          oneKey = find(keyCode);								% use find to figure out which key(s) they pressed
+          keys(t) = oneKey(1);	       % maybe they mashed multiple keys! then just pick one (the first one, in keyCode order)
+          oneKey
+        end
+      end
+      
+      
    
   end
 end
 
+experimentEnd = GetSecs;										                          % time stamp the end of the study (more useful for fMRI/ERP?)
+Screen('CloseAll');												                            % close all the offscreen and onscreen windows
+ShowCursor;														                                % guess what?
+ListenChar(0);         
 
-for t = 1:size(design,1)										% this big loop is for each trial (number of rows in "design")	
-	item(t) = Randi(numImages);									% pick one of the images from the right condition at random
-	Screen('gluDisk', window, 0, ctrPoint(1), ctrPoint(2), 8);  % draw fixation dot (offscreen)
-	vbl = Screen('Flip', window);								% flip it onscreen
-	Screen('DrawTexture', window, ...							% draw an image offscreen in the right location -- try "Screen DrawTexture?" in command window
-	images(design(t,1), item(t)), [], imgPosns{design(t,2)});   % design(t,1) tells us which condition; design(t,2) tells us left or right
-	Screen('gluDisk', window, 0, ctrPoint(1), ctrPoint(2), 8);  % add a fixation offscreen
-	[vbl imgOnset(t) fts(t,1) mis(t,1) beam(t,1)] = ...			% (keep track of lots of Flip output)
-	Screen('Flip', window, vbl + (fixDur .* ifi));		  		% flip the image + fixation onscreen, after the fixation duration is over, and record all timing details from "Flip"
-	Screen('gluDisk', window, 0, ctrPoint(1), ctrPoint(2), 8);  % keep a fixation offscreen	
-	[vbl imgOffset(t) fts(t,2) mis(t,2) beam(t,2)] = ...		% (keep track of lots of Flip output)
-	Screen('Flip', window, ...									% flip again, to replace with a fixation point 
-	vbl + (pixDurs(design(t, 3)) .* ifi));						% after the right duration has elapsed (according to design(t,3))
-	
-	responded = 0; 												% reset the response flag for each trial
-	while ((GetSecs - imgOnset(t)) < maxRespDur)				% keep checking for a keypress until the clock runs out
-		[keyIsDown, secs, keyCode] = KbCheck;					% check for a key press(es)
-		if ~responded && keyIsDown								% if they didn't respond already and a key is down, then ...
-			RTs(t) = GetSecs - imgOnset(t);						% response time, in seconds, since the onset of the image			
-			responded = 1;										% set the flag - now they have responded (so now we won't take any later keypresses on this trial)
-			oneKey = find(keyCode);								% use find to figure out which key(s) they pressed
-			keys(t) = oneKey(1);								% maybe they mashed multiple keys! then just pick one (the first one, in keyCode order)
-		end
-	end
-	
-	if (keys(t) == theKeyCodes(design(t,1)))					% does the keycode match the right key for this condition
-		acc(t) = 1;												% if so set accuracy for that trial to 1
-	end															% otherwise it stays 0 as initialised above
-	
-																% fprintf is a powerful way to output formatted text https://www.cs.utah.edu/~germain/PPS/Topics/Matlab/fprintf.html
-	fprintf(fout, '%s\t%s\t%s\t%3.1f\t%d\t%3.3f\t%d\t%d\t%3.3f\n', ...	
-	subjID, ...													% the part in '' is encoded so that each %s, %d, %f is filled with a string, integer, or float from the remaining arguments
-	condNames(design(t,1),:), ...								% condition name of that trial (string, %s) [all separated by tab characters \t]
-	sideNames(design(t,2),:), ...								% side name of that trial (string, %s)
-	pixDurs(design(t,3)), ...									% duration of that trial (float, %3.1f, 3 digits before and 1 digit after the decimal)
-	keys(t), ...												% keycode of that trial (integer, %d)
-	RTs(t), ...													% response time of that trial (float, %3.3f, 3 digits before and after the decimal)
-	acc(t), ...													% accuracy of that trial (integer, %d)
-	item(t), ...												% which item number was presented on that trial
-	(imgOffset(t) - imgOnset(t)));								% from Flip's estimates of when the target actually appeared and was then removed							
-end
-
-
-
-%%%%%%%%%
-% Clean up at the end!
-%%%%%%%%%
-experimentEnd = GetSecs;										% time stamp the end of the study (more useful for fMRI/ERP?)
-Screen('CloseAll');												% close all the offscreen and onscreen windows
-ShowCursor;														% guess what?
-ListenChar(0);                                                  % reinsates the output of key presses to the command window/editor
-fclose(fout);													% close off the data text file
-save([subjID '_PTBlearn_' datestr(now, 30) '.mat'], '-v7');		% all of the variables are saved in a .mat file; datestamp stops overwriting; '-v7' helps Octave read it
-fprintf('%d misses on key Flip commands.\n', sum(sum(mis>0)));  % based on the "miss" output variable from Flip, on target onset + target offset
-
-%%%%%%%%%
-% A helper function: it randomizes the rows of 2D matrix m, keeping each row intact
-%%%%%%%%%
-function out = dt_randomize(m)									
-  % function out = dt_randomize(m)
-  [R C] = size(m);
-  newInd = randperm(R)';
-  out = zeros(R, C);
-  for i = 1:R
-    out(i, :) = m(newInd(i), :);
-  end
-
-function waitForSpaceBar
-spaceKeyIdx = KbName('space');                          % specify the key to continue the experiment after the break
-[responseTi, keyStateVec] = KbWait;
-KbReleaseWait;                                                                    % hold on until spaceKeyIdx, i.e. space bar, is pressed and then released
-while ~keyStateVec(spaceKeyIdx)                                                   % check the keyboard until the spaceKeyIdx, i.e. space bar, is pressed
-    [~, keyStateVec] = KbWait;
-    KbReleaseWait;
-end
